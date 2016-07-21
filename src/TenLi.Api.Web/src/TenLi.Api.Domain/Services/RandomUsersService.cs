d6a03ae5ce7a3f8b43fef017bdf9aa5ea5650e8b
Anton Dorofeyev <anton@codeoasis.com>
2016-07-21 11:02:23 +0300

    initial commit

---
+++ src/TenLi.Api.Web/src/TenLi.Api.Domain/Services/RandomUsersService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TenLi.Api.Domain.Models;
+
+namespace TenLi.Api.Domain.Services
+{
+    public class RandomUsersGenerator
+    {
+        public RandomUser GenerateRandomUser()
+        {
+            return new RandomUser
+            {
+                Firstname = "Anton",
+                Lastname = "Dorofeyev"
+            };
+        }
+    }
+}
