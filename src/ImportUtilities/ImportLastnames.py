da9cfc681ab0495b02a6a1a556b8c264a66abe93
Anton Dorofeyev <anton@codeoasis.com>
2016-08-10 11:07:04 +0300

    Added import utilities

---
+++ src/ImportUtilities/ImportLastnames.py
@@ -0,0 +1,32 @@
+import csv, json, re, pymongo
+
+from pymongo import MongoClient
+mongoClient = MongoClient('localhost', 27017)
+db = mongoClient.tenli
+lastnamesCollection = db.lastnames
+
+def insert_to_mongo(dto):
+	id = lastnamesCollection.insert_one(dto).inserted_id
+	insertedDoc = lastnamesCollection.find_one({'_id' : id})
+	return insertedDoc
+
+def transmit_csv_data_to_robomow_id():
+
+	with open('Lastnames4Tenli.csv', newline='', encoding='Utf-8') as csvFile:
+
+		reader = csv.DictReader(csvFile, delimiter='\t')
+
+		for row in reader:
+			try:
+				lastnameDto = {
+					'EngValue' : row['EngValue'],
+					'HebValue' : row['HebValue']
+				}
+
+				print(insert_to_mongo(lastnameDto))
+				
+			except Exception as e:
+				print(e)
+				continue
+
+transmit_csv_data_to_robomow_id()
--- src/TenLi.Api.DataAccess/MongoDataAccess.cs
+++ src/TenLi.Api.DataAccess/MongoDataAccess.cs
@@ -3,19 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using MongoDB;
+using Microsoft.Extensions.Configuration;
 
 namespace TenLi.Api.DataAccess
 {
 	public interface IMongoDataAccess
 	{
-
+		string ConnectionString { get; }
 	}
 
     public class MongoDataAccess : IMongoDataAccess
 	{
-        public MongoDataAccess()
-        {
+		private readonly string _mongoConnectionString;
+		private readonly IConfigurationRoot _configuration;
 
-        }
+		public MongoDataAccess(IConfigurationRoot configuration)
+        {
+			_configuration = configuration;
+			_mongoConnectionString = _configuration["MongoDB:ConnectionString"];
+		}
+
+		public string ConnectionString
+		{
+			get { return _mongoConnectionString; }
+		}
     }
 }
